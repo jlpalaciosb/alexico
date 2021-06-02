d2dab04eeeb15162988bc002fc51f2267556c96e
José Luis Palacios <joselupalaciosbo@gmail.com>
2021-06-01 20:40:38 -0400

    afd final

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from automata import NFA, DFA
-from funciones import add_concat_op, conv_rangos, regex_to_nfa, nfa_to_dfa, replaceExpressions
+from funciones import regex_to_nfa, nfa_to_dfa, add_concat_op, conv_rangos, replaceExpressions
 
+
+# input del usuario
 alpha = input('Alfabeto: ')
 n = int(input('Número de definiciones regulares: '))
 print()
-
 print('Ingrese las %d definiciones regulares.' % n)
 defs_regs = [ input() for i in range(n) ]
-defs_regs = replaceExpressions(defs_regs)
 
+
+# preprocesamientos y construcción de los nfas y dfas
+defs_regs = replaceExpressions(defs_regs)
 regexs = []
 for def_reg in defs_regs:
     nombre = def_reg[:(def_reg.index('->') - 1)]
@@ -18,10 +20,26 @@ for def_reg in defs_regs:
     regexs.append({
         'nombre': nombre,
         'regex': regex,
+        'nfa': regex_to_nfa(regex),
         'dfa': nfa_to_dfa(regex_to_nfa(regex))
     })
 print()
 
+
+# construir dfa final
+nfa_final = None
+for regex in regexs:
+    if nfa_final is None:
+        nfa_final = regex['nfa']
+    else:
+        nfa_final = nfa_final.orr(regex['nfa'])
+dfa_final = nfa_to_dfa(nfa_final)
+print('<< DFA FINAL >>')
+dfa_final.print()
+print()
+
+
+# análisis léxico
 cadena = input('Ingrese la cadena a analizar:\n')
 print()
 for lexema in cadena.split():
